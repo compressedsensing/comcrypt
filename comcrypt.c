d3f08435854b4ecf22d692695ac14d0e5fe28ebf
kasperlauge <kasperlaugemadsen@gmail.com>
2020-01-29 16:28:25 +0100

    Implemented block encryption with 128 bit AES

--- comcrypt.c
+++ comcrypt.c
@@ -1,65 +1,57 @@
-/*
- * Copyright (c) 2006, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-
-/**
- * \file
- *         A very simple Contiki application showing how Contiki programs look
- * \author
- *         Adam Dunkels <adam@sics.se>
- */
-
 #include "contiki.h"
+#include <stdio.h>
+#include "lib/aes-128.h"
 
-#include <stdio.h> /* For printf() */
+#define PLAIN_TEXT_SIZE        AES_128_BLOCK_SIZE
+
+void print_text(uint8_t* text, uint8_t length){
+    size_t i;
+    for (i = 0; i < length; i++)
+    {
+        printf("%x", text[i]);
+    }
+    printf("\n");
+}
+
 /*---------------------------------------------------------------------------*/
-PROCESS(hello_world_process, "Hello world process");
-AUTOSTART_PROCESSES(&hello_world_process);
+PROCESS(comcrypt_process, "Comcrypt process");
+AUTOSTART_PROCESSES(&comcrypt_process);
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(hello_world_process, ev, data)
+PROCESS_THREAD(comcrypt_process, ev, data)
 {
-  static struct etimer timer;
+  // 16 = 128 bit key
+  // Key: 8a0439ed 5d393558 9b7c77c8 62a7e135
+  uint8_t key[AES_128_KEY_LENGTH] = { 
+    0x8a, 0x04, 0x39, 0xed,
+    0x5d, 0x39, 0x35, 0x58,
+    0x9b, 0x7c, 0x77, 0xc8,
+    0x62, 0xa7, 0xe1, 0x35 
+  };
+
+  uint8_t plainText[PLAIN_TEXT_SIZE] = {
+    0x11, 0x04, 0x39, 0xed,
+    0x46, 0x56, 0x35, 0x58,
+    0x9b, 0x7c, 0x77, 0xc8,
+    0x62, 0x66, 0xe1, 0x35
+    // 0x8a, 0x04, 0x39, 0xed,
+    // 0x5d, 0x88, 0x35, 0x58,
+    // 0x9b, 0x7c, 0x77, 0xc8,
+    // 0x62, 0xa7, 0xe1, 0x35,
+    // 0x8a, 0x99, 0x39, 0xed,
+    // 0x5d, 0x39, 0x35, 0x58,
+    // 0x9b, 0x7c, 0x77, 0xc8,
+    // 0x62, 0xa7, 0xe1, 0x35 
+  };
 
   PROCESS_BEGIN();
 
-  /* Setup a periodic timer that expires after 10 seconds. */
-  etimer_set(&timer, CLOCK_SECOND * 10);
+  print_text(plainText, PLAIN_TEXT_SIZE);
 
-  while(1) {
-    printf("Hello, world\n");
+  AES_128.set_key(key);
+  AES_128.encrypt(plainText);
 
-    /* Wait for the periodic timer to expire and then restart the timer. */
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-    etimer_reset(&timer);
-  }
+  print_text(plainText, PLAIN_TEXT_SIZE);
+  
 
   PROCESS_END();
 }
