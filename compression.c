1610c789c7f459ec5821326325459e13cc2bb55d
kasperlauge <kasperlaugemadsen@gmail.com>
2020-02-06 16:19:34 +0100

    Added initial steps for huffman code based on codebook

--- compression.c
+++ compression.c
@@ -14,4 +14,36 @@ static void dct_transform(float *input_vector, float *result, unsigned int block
     }
 }
 
+int pushBits(uint16_t huff_code, uint8_t *bitstring, uint16_t bitstring_length) {
+
+}
+
+static struct huffman_data huffman_encode(uint8_t *block, uint16_t length) {
+    uint16_t i;
+    uint8_t firstHalf;
+    uint8_t secondHalf;
+    uint8_t *bitstring;
+    uint16_t last_bit_position = 0;
+    uint16_t huff_code;
+    bitstring = calloc(length, sizeof(uint8_t)); // Potentially N = 512 Bytes
+    for (i = 0; i < length; i++) {
+        // Push first half to final huff code
+        firstHalf = block[i] & 0x0F;
+        huff_code = CODEBOOK[firstHalf];
+        last_bit_position = pushBits(huff_code, bitstring, last_bit_position);
+
+        // Repeat for second half
+        secondHalf = block[i] & 0xF0;
+        huff_code = CODEBOOK[secondHalf];
+        last_bit_position = pushBits(huff_code, bitstring, last_bit_position);
+    }
+
+    struct huffman_data h = {
+        last_bit_position,
+        bitstring
+    };
+
+    return h;
+} 
+
 const struct compression_driver compression_driver = {dct_transform};
--- compression.h
+++ compression.h
@@ -5,7 +5,15 @@
 #include "math.h"
 #include <stddef.h>
 
+#ifdef HUFFMAN_CODEBOOK
+#define CODEBOOK           HUFFMAN_CODEBOOK
+#else /* HUFFMAN_CODEBOOK */
+#define CODEBOOK            huffman_codebook
+#endif /* HUFFMAN_CODEBOOK */
+
 #define COMPRESS compression_driver
+
+#define HUFFMAN_RESOLUTION 4 // Amount of bits to represent symbols
 
 struct compression_driver {
   
@@ -15,6 +23,21 @@ struct compression_driver {
    */
   void (* dct_transform)(float* input_vector,float* res, unsigned int block_size);
 
+};
+
+// Huffman variables
+
+// 4 bit resolution
+extern const uint16_t huffman_codebook[1 << HUFFMAN_RESOLUTION] = {
+  0x4000, 0x4001, 0x2001, 0x1001, 
+  0x801, 0x401, 0x9, 0x0, 
+  0x3, 0x5, 0x201, 0x101, 
+  0x81, 0x41, 0x21, 0x11, 
+};
+
+struct huffman_data {
+  uint16_t length;
+  uint8_t *bits;
 };
 
 
