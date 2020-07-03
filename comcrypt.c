f464fe6ad29a301750838b14cb559eb7d3c5ffcd
kasperlauge <kasperlaugemadsen@gmail.com>
2020-07-03 21:24:42 +0200

    Changed timed test program to also use nullnet

--- comcrypt.c
+++ comcrypt.c
@@ -1,27 +1,20 @@
 #include "contiki.h"
 #include "./encrypt.h"
 #include "./compression.h"
-#include "net/routing/routing.h"
 #include "net/netstack.h"
-#include "net/ipv6/simple-udp.h"
+#include "net/nullnet/nullnet.h"
 #include "sys/ctimer.h"
 #include "sys/log.h"
 #include "./configuration.h"
 #include "./fixedpoint.h"
 
-#define UDP_CLIENT_PORT 8765
-#define UDP_SERVER_PORT 5678
-#define SEND_INTERVAL (5 * CLOCK_SECOND)
+#define TX_BUFFER_SIZE 102
 
-static struct simple_udp_connection udp_conn;
 static struct ctimer timer;
 static uint16_t i = 0;
 static uint8_t state = 0;
 static huffman_metadata h_data;
 static uint8_t signal_bytes[BLOCK_LEN] = {0};
-// static uint8_t *signal_bytes;
-// fe80::212:7400:1a45:c958
-static uip_ipaddr_t dest_ipaddr = {{0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x12, 0x74, 0x00, 0x1a, 0x45, 0xc9, 0x58}};
 
 const huffman_codeword huffman_codebook[16] = {
     {0b1, 1}, {0b0000, 4}, {0b00100, 5}, {0b001111, 6}, {0b001010, 6}, {0b0010111, 7}, {0b0001100, 7}, {0b000110101, 9}, {0b0001101001, 10}, {0b00011011, 8}, {0b0010110, 7}, {0b000111, 6}, {0b001110, 6}, {0b00010, 5}, {0b00110, 5}, {0b01, 2}};
@@ -81,22 +74,6 @@ static const int16_t threshhold = 12; // 0.003 in Q4.12 format
 PROCESS(comcrypt_process, "Comcrypt process");
 AUTOSTART_PROCESSES(&comcrypt_process);
 
-static void
-udp_rx_callback(struct simple_udp_connection *c,
-                const uip_ipaddr_t *sender_addr,
-                uint16_t sender_port,
-                const uip_ipaddr_t *receiver_addr,
-                uint16_t receiver_port,
-                const uint8_t *data,
-                uint16_t datalen)
-{
-  for (i = 0; i < datalen; i++)
-  {
-    LOG_INFO_("%02x", data[i]);
-  }
-  LOG_INFO_("\n");
-}
-
 static void convert_to_bytes() {
   // uint16_t tmp = 0;
   // signal_bytes = (uint8_t*)signal;
@@ -113,17 +90,17 @@ static void convert_to_bytes() {
 }
 
 static void send_packets() {
-  NETSTACK_RADIO.on();
-  uint8_t buf[128] = {0};
+  static uint8_t buf[TX_BUFFER_SIZE] = {0};
+  nullnet_buf = buf;
   #if DEBUG
   LOG_INFO("Sending to receiver mote\n");
   #endif
-  for (i = 0; i <= h_data.byte_length / 128; i++) {
-    memset(buf, 0, 128);
-    memcpy(buf, signal_bytes + (i * 128), i == h_data.byte_length / 128 ? h_data.byte_length % 128 : 128);
-    simple_udp_sendto(&udp_conn, buf, i == h_data.byte_length / 128 ? h_data.byte_length % 128 : 128, &dest_ipaddr);
+  for (i = 0; i < CEIL_DIVIDE(h_data.byte_length,TX_BUFFER_SIZE); i++) {
+    memset(buf, 0, TX_BUFFER_SIZE);
+    memcpy(buf, signal_bytes + (i * TX_BUFFER_SIZE), i == (h_data.byte_length / TX_BUFFER_SIZE) ? h_data.byte_length % TX_BUFFER_SIZE : TX_BUFFER_SIZE);
+    nullnet_len = i == (h_data.byte_length / TX_BUFFER_SIZE) ? h_data.byte_length % TX_BUFFER_SIZE : TX_BUFFER_SIZE;
+    NETSTACK_NETWORK.output(NULL);
   }
-  NETSTACK_RADIO.off();
 }
 
 static void
@@ -186,8 +163,13 @@ callback(void *prt)
     break;
   }
   case 3:
+  {
+    NETSTACK_RADIO.on();
     send_packets();
-    break;
+    // First turn radio off when done with transmission
+    // PROCESS_YIELD();
+    NETSTACK_RADIO.off();
+  }
   default:
     break;
   }
@@ -208,9 +190,6 @@ PROCESS_THREAD(comcrypt_process, ev, data)
     }
     LOG_INFO_("\n");
     #endif
-  /* Initialize UDP connection */
-  simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL,
-                      UDP_SERVER_PORT, udp_rx_callback);
 
   ctimer_set(&timer, 5 * CLOCK_SECOND, callback, NULL);
 
