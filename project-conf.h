90837188914574dcb273081233e41e17c22b0daa
larsh <larshjerrild@hotmail.com>
2020-07-28 14:00:52 +0200

    Merge branch 'master' of https://github.com/compressedsensing/comcrypt

--- project-conf.h
+++ project-conf.h
@@ -1,6 +1,25 @@
 #undef AES_128_CONF
 #define AES_128_CONF  aes_128_driver  
 
+#define QUEUEBUF_CONF_ENABLED 1
+
+// #define CC2420_CONF_CHANNEL 26  // select 15.4 channel for transmission and listening
+// #define CSMA_CONF_MAX_FRAME_RETRIES 0
+#define WITH_PKT_STORAGE 35
+// #define CC2420_CONF_AUTOCRC 0
+// #define CC2420_CONF_CHECKSUM 0
+// #define NONCORESEC_CONF_SEC_LVL 0
+// #define CSMA_CONF_LLSEC_SECURITY_LEVEL 0
+// #define CSMA_CONF_SEND_SOFT_ACK 0
+// #define WITH_SECURITY 0
+// #define LLSEC802154_CONF_ENABLED 0
+// #define PACKETBUF_ATTR_SECURITY_LEVEL 0
+// #define NETSTACK_CONF_MAC nullmac_driver
+// #define NETSTACK_CONF_RDC nullrdc_noframer_driver
+// #define NETSTACK_CONF_FRAMER framer_nullmac
+//#define CC2420_CONF_AUTOACK 0
+
+
 // With uIP stack ---------------------------------
 // #undef NBR_TABLE_CONF_MAX_NEIGHBORS
 // #define NBR_TABLE_CONF_MAX_NEIGHBORS 4
@@ -24,6 +43,9 @@
 // Set the CCA threshold so high that it wont affect measurements
 #undef CC2420_CONF_CCA_THRESH
 #define CC2420_CONF_CCA_THRESH 0
+
+#undef QUEUEBUF_CONF_NUM
+#define QUEUEBUF_CONF_NUM 23
 
 #undef UIP_CONF_UDP
 #define UIP_CONF_UDP 0
